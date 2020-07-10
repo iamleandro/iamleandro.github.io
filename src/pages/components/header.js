04005ac2b06e4f24c8344215f6c403b7d30cdb83
Leandro De Paula <dipaula.sp@gmail.com>
2020-07-10 00:57:00 -0400

    add styles

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import headerStyles from './header.module.scss'
+
 const Header = () => {
     return (
-        <header>
-            <h1>I am Leandro</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                {/* TODO: Add Avatar */}
+                <Link className={headerStyles.title} to='/'>I am Leandro</Link>
+            </h1>
             <nav>
-                <ul>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/blog">Blog</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/projects">Projects</Link>
                     </li>
                     <li>
-                        <Link to="/about-me">About</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about-me">About</Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
                     </li>
                 </ul>
             </nav>
---
+++ src/pages/projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from './components/layout'
+
+const ProjectsPage = () => {
+  return (
+    <Layout>
+      <h1>What I am working on</h1>
+      <p>Coming soon.</p>
+    </Layout>
+  );
+};
+
+export default ProjectsPage;
